58d3997dd82d57224bb1f407e9ce314db56a8846
汪雷 <wangcppclei@gmail.com>
2019-08-19 22:55:52 +0800

    fix dns

--- k8s/k8s/settings.py
+++ k8s/k8s/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import socket
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -122,12 +124,12 @@ STATIC_URL = '/static/'
 
 # Redis
 
-redis_host = 'redis'
+redis_host = socket.gethostbyname('redis-master')
 
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "%s/1" % (redis_host),
+        "LOCATION": "redis://%s/1" % (redis_host),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
